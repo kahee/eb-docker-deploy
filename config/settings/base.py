8f0c32af28da2e2ce1c9aecd31e7947c308e24e0
kahee <yuygh131@gmail.com>
2018-03-13 14:16:26 +0900

    migrate 적용

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '.media')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
 # static

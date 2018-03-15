cb230f960ec00f3458813377414bcea8543ab201
kahee <yuygh131@gmail.com>
2018-03-15 12:29:08 +0900

    static 설정 변경

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -17,7 +17,7 @@ INSTALLED_APPS += [
 set_config(secrets, module_name=__name__, start=True)
 # print(getattr(sys.modules[__name__], 'DATABASES'))
 
-# Media(user-uploaded file)을 위한 스토리지
-DEFAULT_FILE_STORAGE = 'config.storage.DefaultFileStorage'
-# Static files(collectstatic) 을 위한 스토리지
-STATICFILES_STORAGE = 'config.storage.StaticFileStorage'
+# # Media(user-uploaded file)을 위한 스토리지
+# DEFAULT_FILE_STORAGE = 'config.storage.DefaultFileStorage'
+# # Static files(collectstatic) 을 위한 스토리지
+# STATICFILES_STORAGE = 'config.storage.StaticFileStorage'
